bce3a84514a0fc898410351fc98706771ddd8ac0
Maxim Shchetynin <maxim@zeppelinium.de>
2018-05-27 18:09:00 +0200

    Configuration erweitert

---
+++ Configuration.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define CONFIG_SENSORS_LIST             "Sensors"
+#define CONFIG_SENSORS_THERMA           "Therma"
+#define CONFIG_SENSORS_THERMA_ID        "Id"
+#define CONFIG_SENSORS_THERMA_NAME      "Name"
+#define CONFIG_SENSORS_THERMA_MODBUS_ID "MODBUS"
+
+using namespace std;
+
+namespace Configuration
+{
+    const unsigned MODBUSPortNumber = 502;
+};
