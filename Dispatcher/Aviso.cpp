7cd976a95f8355897e62a7b1fcf2ba44b3f0fac5
Maxim Shchetynin <maxim@zeppelinium.de>
2018-05-28 13:16:36 +0200

    Weitere Aviso Typen

--- Dispatcher/Aviso.cpp
+++ Dispatcher/Aviso.cpp
@@ -11,16 +11,18 @@
 //
 #include "Servus/Dispatcher/Aviso.hpp"
 
+Dispatcher::Aviso::Aviso(const std::string& avisoType) :
+avisoType(avisoType)
+{
+    this->avisoId = 0;
+
+    this->timestamp = new Toolkit::Timestamp();
+}
+
 Dispatcher::Aviso::Aviso(
-    const std::string&      stamp,
-    const std::string&      fabulatorName,
-    const unsigned short    severityLevel,
-    const bool              notificationFlag,
-    const std::string&      message) :
-fabulatorName(fabulatorName),
-severityLevel(severityLevel),
-notificationFlag(notificationFlag),
-message(message)
+    const std::string&  avisoType,
+    const std::string&  stamp) :
+avisoType(avisoType)
 {
     this->avisoId = 0;
 
@@ -40,12 +42,49 @@ Dispatcher::Aviso::~Aviso()
 }
 
 void
-Dispatcher::Aviso::prepare(RTSP::Datagram& datagram)
+Dispatcher::Aviso::prepare(RTSP::Datagram& datagram) const
 {
     datagram["Aviso-Id"]        = this->avisoId;
     datagram["Timestamp"]       = this->timestamp->floatString();
+}
+
+Dispatcher::FabulaAviso::FabulaAviso(
+    const std::string&      stamp,
+    const std::string&      fabulatorName,
+    const unsigned short    severityLevel,
+    const bool              notificationFlag,
+    const std::string&      message) :
+Inherited("FABULA", stamp),
+fabulatorName(fabulatorName),
+severityLevel(severityLevel),
+notificationFlag(notificationFlag),
+message(message)
+{ }
+
+void
+Dispatcher::FabulaAviso::prepare(RTSP::Datagram& datagram) const
+{
+    Inherited::prepare(datagram);
+
     datagram["Severity"]        = this->severityLevel;
     datagram["Notification"]    = this->notificationFlag;
     datagram["Originator"]      = this->fabulatorName;
     datagram["Message"]         = this->message;
+}
+
+Dispatcher::TemperatureAviso::TemperatureAviso(
+    const std::string&  sensorToken,
+    const float         temperature) :
+Inherited("TEMPERATURE"),
+sensorToken(sensorToken),
+temperature(temperature)
+{ }
+
+void
+Dispatcher::TemperatureAviso::prepare(RTSP::Datagram& datagram) const
+{
+    Inherited::prepare(datagram);
+
+    datagram["Sensor-Token"]    = this->sensorToken;
+    datagram["Temperature"]     = this->temperature;
 }
--- Dispatcher/Aviso.hpp
+++ Dispatcher/Aviso.hpp
@@ -16,25 +16,59 @@ namespace Dispatcher
     {
     public:
         unsigned int        avisoId;
+        std::string         avisoType;
 
         Toolkit::Timestamp* timestamp;
 
+    public:
+        Aviso(const std::string& avisoType);
+
+        Aviso(
+            const std::string&  avisoType,
+            const std::string&  stamp);
+
+        ~Aviso();
+
+        virtual void
+        prepare(RTSP::Datagram&) const;
+    };
+
+    class FabulaAviso : public Dispatcher::Aviso
+    {
+        typedef Dispatcher::Aviso Inherited;
+
+    public:
         std::string         fabulatorName;
         unsigned short      severityLevel;
         bool                notificationFlag;
         std::string         message;
 
     public:
-        Aviso(
+        FabulaAviso(
             const std::string&      stamp,
             const std::string&      fabulatorName,
             const unsigned short    severityLevel,
             const bool              notificationFlag,
             const std::string&      message);
 
-        ~Aviso();
+        virtual void
+        prepare(RTSP::Datagram&) const;
+    };
 
-        void
-        prepare(RTSP::Datagram&);
+    class TemperatureAviso : public Dispatcher::Aviso
+    {
+        typedef Dispatcher::Aviso Inherited;
+
+    public:
+        std::string sensorToken;
+        float       temperature;
+
+    public:
+        TemperatureAviso(
+            const std::string&  sensorToken,
+            const float         temperature);
+
+        virtual void
+        prepare(RTSP::Datagram&) const;
     };
 };
